24ff32455396da7e577fde86f94e707f2c883a64
Arth Thakkar <arththakkar@gmail.com>
2018-10-18 12:06:14 +0530

    api added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,17 @@ class HomeController < ApplicationController
 	def index
 		@collections = Collection.all.order(:name)
 	end
+
+	def list_of_collections
+		collections = Collection.all.order(:name)
+		render :json => collections.to_json
+	end
+
+	def list_of_images_by_collection
+		collection_images = {}
+		Collection.all.each do |c|
+			collection_images[c.id] = c.collection_images
+		end
+		render :json => collection_images.to_json
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index'
+  get "/list_of_collections", to: "home#list_of_collections", as: :list_of_collections
+  get "/list_of_images_by_collection", to: "home#list_of_images_by_collection", as: :list_of_images_by_collection
 end
