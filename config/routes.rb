eb33a501201a7e8304b65061aa0b8e88e1c09bf5
Arth Thakkar <arththakkar@gmail.com>
2018-11-15 19:51:56 +0530

    Route for feedback

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   root 'home#index'
   get "/list_of_collections", to: "home#list_of_collections", as: :list_of_collections
   get "/list_of_images_by_collection", to: "home#list_of_images_by_collection", as: :list_of_images_by_collection
+  post "/feedback", to: "home#feedback", as: :feedback
 end
